199dde30dc281d858738c2d6d8e1ffc72785622c
Osamikoyo <v.pylayev@mail.ru>
2025-07-31 08:13:58 +0500

    add adori core

---
+++ core/core.go
@@ -0,0 +1,77 @@
+package core
+
+import (
+	"net/http"
+
+	"github.com/osamikoyo/adori/cash"
+	"github.com/osamikoyo/adori/defence"
+	"github.com/osamikoyo/adori/logger"
+	"github.com/osamikoyo/adori/statistic"
+	"go.uber.org/zap"
+)
+
+type AdoriCore struct {
+	cash      *cash.LocalCash
+	defence   *defence.Defence
+	statistic *statistic.StatisticClient
+
+	logger *logger.Logger
+}
+
+func NewAdoriCore(
+	cash *cash.LocalCash,
+	defence *defence.Defence,
+	statistic *statistic.StatisticClient,
+	logger *logger.Logger,
+) *AdoriCore {
+	return &AdoriCore{
+		cash: cash,
+		defence: defence,
+		statistic: statistic,
+		logger: logger,
+	}
+}
+
+func (ac *AdoriCore) CoreMiddlewareForHandlerFunc(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var status string
+
+		defer ac.statistic.AddChunk(r, status)
+
+		if !ac.defence.CheckRequestOK(r) {
+			w.Write([]byte("bad request"))
+
+			status = "bad"
+
+			return
+		}
+
+		status = "ok"
+
+		ac.logger.Info("new good request", zap.String("path", r.URL.Path))
+
+		handler.ServeHTTP(w, r)
+	}
+}
+
+func (ac *AdoriCore) CoreMiddlewareForHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){ 
+		var status string
+
+		defer ac.statistic.AddChunk(r, status)
+
+		if !ac.defence.CheckRequestOK(r) {
+			w.Write([]byte("bad request"))
+
+			status = "bad"
+
+			return
+		}
+
+		status = "ok"
+
+		ac.logger.Info("new good request", zap.String("path", r.URL.Path))
+
+		handler.ServeHTTP(w, r)
+	})
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
